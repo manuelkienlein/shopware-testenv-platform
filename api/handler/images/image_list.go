654ee6eb13c0da4f153d78645a4c8d72acda72b8
Manuel Kienlein <manuel.kienlein@mr-pixel.de>
2024-12-19 11:11:37 +0100

    Implemented handler for listing images

--- api/handler/images/image_list.go
+++ api/handler/images/image_list.go
@@ -1,8 +1,12 @@
 package images
 
 import (
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/client"
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"strings"
+	"time"
 )
 
 // ImageListHandler lists all images
@@ -16,15 +20,42 @@ import (
 // @Router /api/images [get]
 func (h *ImageHandler) ImageListHandler(c echo.Context) error {
 
-	// TODO implement
+	ctx := c.Request().Context()
 
-	output := []Image{{
-		ID:        "containerID",
-		ImageName: "Name",
-		ImageTag:  "6.0.0.0",
-		CreatedAt: "2013-08-20T18:52:09.000Z",
-		Size:      123,
-	}}
+	// Get docker client
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	defer cli.Close()
+
+	// List images
+	images, err := cli.ImageList(ctx, image.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	var output []Image
+
+	for _, image := range images {
+
+		// Extract id
+		imageHash := strings.Split(image.ID, ":")[1]
+
+		// Extract image name (only uses first)
+		imageName := strings.Split(image.RepoTags[0], ":")[0]
+
+		// Extract image tag (only uses first)
+		imageTag := strings.Split(image.RepoTags[0], ":")[1]
+
+		output = append(output, Image{
+			ID:        imageHash,
+			ImageName: imageName,
+			ImageTag:  imageTag,
+			CreatedAt: time.Unix(image.Created, 0).Format(time.RFC3339),
+			Size:      image.Size,
+		})
+	}
 
 	return c.JSON(http.StatusOK, output)
 }
