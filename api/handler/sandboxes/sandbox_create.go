5488180246e11c8eac26a5cc293195a66cb82d41
Manuel Kienlein <manuel.kienlein@mr-pixel.de>
2024-12-19 11:11:01 +0100

    Refactored sandbox handlers

--- api/handler/sandboxes/sandbox_create.go
+++ api/handler/sandboxes/sandbox_create.go
@@ -1,1 +1,78 @@
 package sandboxes
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+type SandboxCreateRequest struct {
+	ImageId  string `json:"image_id" example:"dockware/dev"`
+	Lifetime int    `json:"lifetime" example:"1440"`
+}
+
+type SandboxCreateResponse struct {
+	Status        string `json:"status" example:"success"`
+	Message       string `json:"message" example:"Sandbox created successfully"`
+	Image         string `json:"image" example:"dockware/dev:6.6.8.2"`
+	ContainerName string `json:"container_name" example:"sandbox-container"`
+	ContainerId   string `json:"container_id" example:"sandbox-container-id"`
+	Url           string `json:"url" example:"https://sandbox-random.shopshredder.zion.mr-pixel.de"`
+}
+
+// SandboxCreateHandler creates a new sandbox for requested image
+// @Summary Create new sandbox
+// @Description Creates a new sandbox docker container
+// @Tags Sandbox Management
+// @Accept json
+// @Produce json
+// @Param image body SandboxCreateRequest true "Image Input"
+// @Success 200 {object} SandboxCreateResponse
+// @Failure 400 {object} map[string]string
+// @Router /api/sandboxes [post]
+func (h *SandboxHandler) SandboxCreateHandler(c echo.Context) error {
+
+	// TODO implement
+	ctx := c.Request().Context()
+	var input SandboxCreateRequest
+
+	// Parse input
+	if err := c.Bind(&input); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"error": "invalid request format",
+		})
+	}
+
+	// Input validation
+	if input.Lifetime > 1440 {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"error": "Lifetime should be less than 1440",
+		})
+	}
+
+	if input.Lifetime < 5 {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"error": "Lifetime should be greater than 5",
+		})
+	}
+
+	// TODO: other data
+	imageName := "dockware/dev:6.6.8.2"
+	instanceName := "sandbox_random"
+	host := "sandbox-random.shopshredder.zion.mr-pixel.de"
+
+	containerID, err := h.DockerService.CreateContainer(ctx, imageName, instanceName, host)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	output := SandboxCreateResponse{
+		Message:       "Sandbox created successfully",
+		Status:        "success",
+		ContainerName: instanceName,
+		ContainerId:   containerID,
+		Image:         imageName,
+		Url:           host,
+	}
+
+	return c.JSON(http.StatusOK, output)
+}
--- api/handler/sandboxes/sandbox_delete.go
+++ api/handler/sandboxes/sandbox_delete.go
@@ -1,1 +1,60 @@
 package sandboxes
+
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"log"
+	"net/http"
+)
+
+type SandboxDeleteResponse struct {
+	Status  string `json:"status" example:"success"`
+	Message string `json:"message" example:"Docker Image removed successfully"`
+}
+
+// SandboxDeleteHandler removes a docker sandbox
+// @Summary Remove Sandbox Environment
+// @Description Removes a sandbox docker container
+// @Tags Sandbox Management
+// @Accept json
+// @Produce json
+// @Param id path string true "Container ID"
+// @Success 200 {object} SandboxDeleteResponse
+// @Failure 400 {object} map[string]string
+// @Router /api/sandboxes/{id} [delete]
+func (h *SandboxHandler) SandboxDeleteHandler(c echo.Context) error {
+
+	ctx := c.Request().Context()
+	containerID := c.Param("id")
+
+	// TODO implement
+
+	log.Println("Stopping sandbox container " + containerID)
+
+	containerInfos, err := h.DockerService.ListContainers(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containerInfos {
+		fmt.Println("ContainerName " + container.Name)
+		fmt.Println("ContainerID " + container.ID)
+		// TODO: Only delete container based on ID
+		if container.Name == "/shopware_6.6" {
+			fmt.Print("Stopping container ", container.ID[:10], "... ")
+
+			if err := h.DockerService.StopContainer(ctx, container.ID); err != nil {
+				return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+			}
+
+			fmt.Println("Success")
+		}
+	}
+
+	output := SandboxDeleteResponse{
+		Message: "Sandbox " + containerID + " removed successfully",
+		Status:  "success",
+	}
+
+	return c.JSON(http.StatusOK, output)
+}
